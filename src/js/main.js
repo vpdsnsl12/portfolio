028434292685ddc3e8cd1d59e9735d0bb835a7b1
vpdsnsl12 <vpdsnsl123@naver.com>
2024-04-24 17:53:51 +0900

    기본환경설정(폰트,색상,이미지)

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import { intro } from "./intro.js";
+import { smooth } from "./smooth.js";
+import { work } from "./work.js";
+import { project } from "./project.js";
+
+
+
+window.addEventListener('load',function(){
+  smooth()
+  project()
+  intro()
+  work()
+})
