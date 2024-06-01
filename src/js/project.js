e73625ff7b311546a1d69cef722c1ac5f599870b
Park Juhee <vpdsnsl123@naver.com>
2024-06-01 19:07:31 +0900

    링크 연결 수정

--- src/js/project.js
+++ src/js/project.js
@@ -1,33 +1,3 @@
-// export function project() {
-//   const swiper3 = new Swiper(".mySwiper3", {
-//     effect: "cube",
-//     grabCursor: true,
-//     cubeEffect: {
-//       shadow: true,
-//       slideShadows: true,
-//       shadowOffset: 20,
-//       shadowScale: 0.94,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     on: {
-//       slideChange: function () {
-//         const activeIndex = this.activeIndex;
-//         const textBoxes = document.querySelectorAll(".text-box");
-//         textBoxes.forEach((textBox, index) => {
-//           if (index -1 === activeIndex) {
-//             textBox.classList.add("on");
-//           } else {
-//             textBox.classList.remove("on");
-//           }
-//         });
-//       },
-//     },
-//   });
-// }
-
 export function project() {
   const swiper3 = new Swiper(".mySwiper3", {
     effect: "cube",
@@ -62,20 +32,20 @@ export function project() {
         // 현재 슬라이드 인덱스에 따라 링크 업데이트
         switch (activeIndex) {
           case 0:
-            linkElement.href = 'https://vpdsnsl12.github.io/trip/';
-            linkElement.textContent = '여기어때 바로가기';
+            linkElement.href = 'https://vpdsnsl12.github.io/portfolio/';
+            linkElement.textContent = '현재 페이지 입니다';
             break;
           case 1:
             linkElement.href = 'https://www.figma.com/proto/WG62Soxgb7TChhNgz58BIU/%EA%B0%9C%EA%B3%A8%EA%B0%9C%EA%B3%A8(Get-Goal)?node-id=1318-929&t=wj2A95JVg8QHHC22-0&scaling=scale-down&page-id=1%3A5';
             linkElement.textContent = '피그마 프로토타입';
             break;
           case 2:
-            linkElement.href = 'https://vpdsnsl12.github.io/portfolio/';
-            linkElement.textContent = '현재 페이지 입니다';
-            break;
-          default:
             linkElement.href = 'https://vpdsnsl12.github.io/trip/';
             linkElement.textContent = '여기어때 바로가기';
+            break;
+          default:
+            linkElement.href = 'https://vpdsnsl12.github.io/portfolio/';
+            linkElement.textContent = '현재 페이지 입니다';
         }
 
         console.log("Link Element After Update: ", linkElement.href); // 디버깅 로그
